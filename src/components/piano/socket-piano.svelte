a5a9768ba9073c2b89b29d0f929edb569ac2b0d4
rhysw97 <62967056+rhysw97@users.noreply.github.com>
2023-02-07 15:31:05 +0000

    changed piano components to use typescript to set up p5 sketch correctly

--- src/components/piano/socket-piano.svelte
+++ src/components/piano/socket-piano.svelte
@@ -1,16 +1,20 @@
-<script>
+<script lang="ts">
     import {Hand} from '../../handpiano-scripts/hand'
     import {SocketPiano} from '../../handpiano-scripts/socket-piano'
     import {pianoSketch} from '../../handpiano-scripts/sketch'
     import { onMount } from 'svelte'
     export let id;
     export let name;
+    import p5 from 'p5'
+    interface PianoSketch extends p5 {
+        piano?: SocketPiano
+    }
 
     onMount (() => {
         console.log(document.getElementById("pianoContainer"))
         let piano = new SocketPiano(name, id);
        // console.log(piano)
-        const pianoScreen = new p5(pianoSketch)
+        const pianoScreen : PianoSketch = new p5(pianoSketch)
         pianoScreen.piano = piano
        // console.log(pianoScreen.piano)
     //giving values to empty p5 sketch properties
